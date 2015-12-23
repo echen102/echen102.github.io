fee26a21f5593ed3ee08961dc279f471b1511bf4
EmilyChen <echen33@illinois.edu>
2015-12-23 16:41:30 -0600

    mp3 stuff

---
+++ cs418/HelloTerrain1.js
@@ -0,0 +1,408 @@
+
+var gl;
+var canvas;
+var shaderProgram;
+var vertexPositionBuffer;
+// emily variables
+var lastTime = 0;
+var move = 0;
+var rotAngle = 0;
+
+// Create a place to store terrain geometry
+var tVertexPositionBuffer;
+
+//Create a place to store normals for shading
+var tVertexNormalBuffer;
+
+// Create a place to store the terrain triangles
+var tIndexTriBuffer;
+
+//Create a place to store the traingle edges
+var tIndexEdgeBuffer;
+
+// View parameters
+var eyePt = vec3.fromValues(0.0,0.0,0.0);
+var viewDir = vec3.fromValues(0.0,0.0,0.0);
+var up = vec3.fromValues(0.0,1.0,0.0);
+var viewPt = vec3.fromValues(0.0,0.0,0.0);
+
+// Create the normal
+var nMatrix = mat3.create();
+
+// Create ModelView matrix
+var mvMatrix = mat4.create();
+
+//Create Projection matrix
+var pMatrix = mat4.create();
+
+var mvMatrixStack = [];
+
+var keysDown = {};
+//var quat = quat.create();
+var multQuat = quat.create();
+
+
+//-------------------------------------------------------------------------
+function setupTerrainBuffers() {
+    
+    var vTerrain=[];
+    var fTerrain=[];
+    var nTerrain=[];
+    var eTerrain=[];
+    var gridN=20;
+    
+    // sets size for terrain 
+    var numT = terrainFromIteration(gridN, -4,4,-4,4, vTerrain, fTerrain, nTerrain);
+    console.log("Generated ", numT, " triangles"); 
+    tVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tVertexPositionBuffer);      
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vTerrain), gl.STATIC_DRAW);
+    tVertexPositionBuffer.itemSize = 3;
+    tVertexPositionBuffer.numItems = (gridN+1)*(gridN+1);
+    
+    // Specify normals to be able to do lighting calculations
+    tVertexNormalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tVertexNormalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(nTerrain),
+                  gl.STATIC_DRAW);
+    tVertexNormalBuffer.itemSize = 3;
+    tVertexNormalBuffer.numItems = (gridN+1)*(gridN+1);
+    
+    // Specify faces of the terrain 
+    tIndexTriBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tIndexTriBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(fTerrain),
+                  gl.STATIC_DRAW);
+    tIndexTriBuffer.itemSize = 1;
+    tIndexTriBuffer.numItems = numT*3;
+    
+    //Setup Edges
+     generateLinesFromIndexedTriangles(fTerrain,eTerrain);  
+     tIndexEdgeBuffer = gl.createBuffer();
+     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tIndexEdgeBuffer);
+     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(eTerrain),
+                  gl.STATIC_DRAW);
+     tIndexEdgeBuffer.itemSize = 1;
+     tIndexEdgeBuffer.numItems = eTerrain.length;
+    
+     
+}
+
+//-------------------------------------------------------------------------
+function drawTerrain(){
+ gl.polygonOffset(0,0);
+ gl.bindBuffer(gl.ARRAY_BUFFER, tVertexPositionBuffer);
+ gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, tVertexPositionBuffer.itemSize, 
+                         gl.FLOAT, false, 0, 0);
+
+ // Bind normal buffer
+ gl.bindBuffer(gl.ARRAY_BUFFER, tVertexNormalBuffer);
+ gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
+                           tVertexNormalBuffer.itemSize,
+                           gl.FLOAT, false, 0, 0);   
+    
+ //Draw 
+ gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tIndexTriBuffer);
+ gl.drawElements(gl.TRIANGLES, tIndexTriBuffer.numItems, gl.UNSIGNED_SHORT,0);      
+}
+
+//-------------------------------------------------------------------------
+function drawTerrainEdges(){
+ gl.polygonOffset(1,1);
+ gl.bindBuffer(gl.ARRAY_BUFFER, tVertexPositionBuffer);
+ gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, tVertexPositionBuffer.itemSize, 
+                         gl.FLOAT, false, 0, 0);
+
+ // Bind normal buffer
+ gl.bindBuffer(gl.ARRAY_BUFFER, tVertexNormalBuffer);
+ gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
+                           tVertexNormalBuffer.itemSize,
+                           gl.FLOAT, false, 0, 0);   
+    
+ //Draw 
+ gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tIndexEdgeBuffer);
+ gl.drawElements(gl.LINES, tIndexEdgeBuffer.numItems, gl.UNSIGNED_SHORT,0);      
+}
+
+//-------------------------------------------------------------------------
+function uploadModelViewMatrixToShader() {
+  gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+//-------------------------------------------------------------------------
+function uploadProjectionMatrixToShader() {
+  gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, 
+                      false, pMatrix);
+}
+
+//-------------------------------------------------------------------------
+function uploadNormalMatrixToShader() {
+  mat3.fromMat4(nMatrix,mvMatrix);
+  mat3.transpose(nMatrix,nMatrix);
+  mat3.invert(nMatrix,nMatrix);
+  gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, nMatrix);
+}
+
+//----------------------------------------------------------------------------------
+function mvPushMatrix() {
+    var copy = mat4.clone(mvMatrix);
+    mvMatrixStack.push(copy);
+}
+
+
+//----------------------------------------------------------------------------------
+function mvPopMatrix() {
+    if (mvMatrixStack.length == 0) {
+      throw "Invalid popMatrix!";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
+
+//----------------------------------------------------------------------------------
+function setMatrixUniforms() {
+    uploadModelViewMatrixToShader();
+    uploadNormalMatrixToShader();
+    uploadProjectionMatrixToShader();
+}
+
+
+//----------------------------------------------------------------------------------
+function degToRad(degrees) {
+        return degrees * Math.PI / 180;
+}
+
+//----------------------------------------------------------------------------------
+function createGLContext(canvas) {
+  var names = ["webgl", "experimental-webgl"];
+  var context = null;
+  for (var i=0; i < names.length; i++) {
+    try {
+      context = canvas.getContext(names[i]);
+    } catch(e) {}
+    if (context) {
+      break;
+    }
+  }
+  if (context) {
+    context.viewportWidth = canvas.width;
+    context.viewportHeight = canvas.height;
+  } else {
+    alert("Failed to create WebGL context!");
+  }
+  return context;
+}
+
+//----------------------------------------------------------------------------------
+function loadShaderFromDOM(id) {
+  var shaderScript = document.getElementById(id);
+  
+  // If we don't find an element with the specified id
+  // we do an early exit 
+  if (!shaderScript) {
+    return null;
+  }
+  
+  // Loop through the children for the found DOM element and
+  // build up the shader source code as a string
+  var shaderSource = "";
+  var currentChild = shaderScript.firstChild;
+  while (currentChild) {
+    if (currentChild.nodeType == 3) { // 3 corresponds to TEXT_NODE
+      shaderSource += currentChild.textContent;
+    }
+    currentChild = currentChild.nextSibling;
+  }
+ 
+  var shader;
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    return null;
+  }
+ 
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+ 
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader));
+    return null;
+  } 
+  return shader;
+}
+
+//----------------------------------------------------------------------------------
+function setupShaders() {
+  vertexShader = loadShaderFromDOM("shader-vs");
+  fragmentShader = loadShaderFromDOM("shader-fs");
+  
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Failed to setup shaders");
+  }
+
+  gl.useProgram(shaderProgram);
+
+  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+  shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+  gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+
+  shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+  shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+  shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
+  shaderProgram.uniformLightPositionLoc = gl.getUniformLocation(shaderProgram, "uLightPosition");    
+  shaderProgram.uniformAmbientLightColorLoc = gl.getUniformLocation(shaderProgram, "uAmbientLightColor");  
+  shaderProgram.uniformDiffuseLightColorLoc = gl.getUniformLocation(shaderProgram, "uDiffuseLightColor");
+  shaderProgram.uniformSpecularLightColorLoc = gl.getUniformLocation(shaderProgram, "uSpecularLightColor");
+}
+
+
+//-------------------------------------------------------------------------
+function uploadLightsToShader(loc,a,d,s) {
+  gl.uniform3fv(shaderProgram.uniformLightPositionLoc, loc);
+  gl.uniform3fv(shaderProgram.uniformAmbientLightColorLoc, a);
+  gl.uniform3fv(shaderProgram.uniformDiffuseLightColorLoc, d);
+  gl.uniform3fv(shaderProgram.uniformSpecularLightColorLoc, s);
+}
+
+//----------------------------------------------------------------------------------
+function setupBuffers() {
+    setupTerrainBuffers();
+}
+function degToRad(degrees) {
+        return degrees * Math.PI / 180;
+}
+//----------------------------------------------------------------------------------
+function draw() { 
+
+    var transformVec = vec3.create();
+  
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // We'll use perspective 
+    mat4.perspective(pMatrix,degToRad(45), gl.viewportWidth / gl.viewportHeight, 0.1, 200.0);
+
+    // We want to look down -z, so create a lookat point in that direction    
+    vec3.add(viewPt, eyePt, viewDir );
+    
+    // Then generate the lookat matrix and initialize the MV matrix to that view
+    mat4.lookAt(mvMatrix,eyePt,viewPt,up);    
+ 
+    //Draw Terrain
+    mvPushMatrix();
+    
+    // can use this to "move" around the terrain 
+    // really just moving the terrain closer to the camera
+    vec3.set(transformVec,0,-0.25,-7);
+
+  
+    // quaternion to apply to transform vector
+    //quat.rotateY(multQuat, multQuat, degToRad(0.1));
+   // quat.rotateX(multQuat, multQuat, degToRad(-0.3));
+    // quat.rotateZ(multQuat, multQuat, degToRad(1));
+
+    quat.normalize(multQuat, multQuat);
+    vec3.transformQuat(transformVec, transformVec, multQuat);
+    
+    var input_vec = vec3.fromValues();
+    
+    // transform with quaternion that i have set
+    mat4.translate(mvMatrix, mvMatrix,transformVec);
+    // X changes the "height" that the eye is to the plane
+    mat4.rotateX(mvMatrix, mvMatrix, degToRad(-90));
+    
+    // rotates the plane - positive rotAngle val rotates left
+    // negative rotAngle val rotates right 
+    mat4.rotateZ(mvMatrix, mvMatrix, degToRad(90));   
+
+    setMatrixUniforms();
+    
+    if ((document.getElementById("polygon").checked) || (document.getElementById("wirepoly").checked))
+    {
+        
+      uploadLightsToShader([0,1,1],[0.0,0.0,0.0],[1.0,0.5,0.0],[0.0,0.0,0.0]);
+      drawTerrain();
+    }
+    if(document.getElementById("wirepoly").checked){
+      uploadLightsToShader([0,1,1],[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]);
+      drawTerrainEdges();
+    }
+
+    if(document.getElementById("wireframe").checked){
+      uploadLightsToShader([0,1,1],[1.0,1.0,1.0],[0.0,0.0,0.0],[0.0,0.0,0.0]);
+      drawTerrainEdges();
+    }
+    mvPopMatrix();
+  
+}
+
+//----------------------------------------------------------------------------------
+function animate() {
+    eyePt[0]+=0.1;
+       if (keysDown[37])
+    {
+        eyePt[1]+=0.1;
+    quat.rotateY(multQuat, multQuat, degToRad(-0.5));
+      //  var eyePt = vec3.fromValues(0,0,0);
+        console.log("hi");
+
+        
+    }
+    if (keysDown[39] )
+    {
+        eyePt[1]-=0.1;
+        console.log("hi~");
+
+        quat.rotateY(multQuat, multQuat, degToRad(0.5));
+        
+    }   
+    var timeNow = new Date().getTime();
+    if (lastTime != 0) {
+        var elapsed = timeNow - lastTime;   
+        
+        // speed at which the plane is constantly flying forward
+        move = move + 0.1;
+        rotAngle= (rotAngle+0.3) % 360;
+
+    }
+    lastTime = timeNow;
+}
+
+//----------------------------------------------------------------------------------
+function startup() {
+  canvas = document.getElementById("myGLCanvas");
+  gl = createGLContext(canvas);
+  setupShaders();
+  setupBuffers();
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+  tick();
+}
+
+function handleKeyDown(event)
+{
+    keysDown[event.keyCode] = true;   
+}
+
+function handleKeyUp(event)
+{
+    keysDown[event.keyCode] = false;
+}
+//----------------------------------------------------------------------------------
+function tick() {
+    requestAnimFrame(tick);
+    draw();
+    animate();
+}
+
